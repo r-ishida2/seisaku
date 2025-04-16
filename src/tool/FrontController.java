909e3676dfadeda90bf12edce067b823745a6c3d
t-enoki63 <fko2447055@stu.o-hara.ac.jp>
2025-04-16 12:00:44 +0900

    furo

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -24,7 +24,6 @@ public class FrontController extends HttpServlet {
     public void doPost(
         HttpServletRequest request, HttpServletResponse response
     ) throws ServletException, IOException {
-    	System.out.println("Frontcontroller!");
         PrintWriter out = response.getWriter();
         try {
             // ① リクエストされたURLのパスを取得
