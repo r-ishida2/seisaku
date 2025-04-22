5a5c51b73136342f633a681fc4a8bc22c2602d94
t-enoki63 <fko2447055@stu.o-hara.ac.jp>
2025-04-22 12:38:48 +0900

    Test,Beanのみ。

---
+++ src/bean/Test.java
@@ -0,0 +1,48 @@
+package bean;
+
+public class Test {
+private Student student;
+private String classnum;
+private Subject subject;
+private School school;
+private int no;
+private int point;
+public Student getStudent() {
+	return student;
+}
+public void setStudent(Student student) {
+	this.student = student;
+}
+public String getClassNum() {
+	return classnum;
+}
+public void setClassNum(String classnum) {
+	this.classnum = classnum;
+}
+public Subject getSubject() {
+	return subject;
+}
+public void setSubject(Subject subject) {
+	this.subject = subject;
+}
+public School getSchool() {
+	return school;
+}
+public void setSchool(School school) {
+	this.school = school;
+}
+public int getNo() {
+	return no;
+}
+public void setNo(int no) {
+	this.no = no;
+}
+public int getPoint() {
+	return point;
+}
+public void setPoint(int point) {
+	this.point = point;
+}
+
+
+}
