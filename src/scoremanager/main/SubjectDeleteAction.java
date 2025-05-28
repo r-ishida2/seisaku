993edecd14b3400992e57a6f4b8992c570c7dc8a
t-enoki63 <fko2447055@stu.o-hara.ac.jp>
2025-05-28 14:42:25 +0900

    Delete

---
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,28 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.School;
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+    public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        String cd = req.getParameter("cd");
+
+        // セッションからログイン中の教師を取得
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("NAME");
+        School school = teacher.getSchool();
+
+        SubjectDao dao = new SubjectDao();
+        Subject subject = dao.get(cd, school); // School オブジェクトを渡す
+
+        req.setAttribute("subject", subject);
+        return "subject_delete.jsp";
+    }
+}
---
+++ src/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -0,0 +1,31 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.School;
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteExecuteAction extends Action {
+    public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        req.setCharacterEncoding("UTF-8");
+        String cd = req.getParameter("cd");
+
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("NAME");
+        School school = teacher.getSchool();
+
+        Subject subject = new Subject();
+        subject.setCd(cd);
+        subject.setSchool(school); // School をセット
+
+        SubjectDao dao = new SubjectDao();
+        dao.delete(subject);
+
+        return "subject_delete_done.jsp";
+    }
+}
